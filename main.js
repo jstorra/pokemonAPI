fd694572a37d6a5482f950ffdebc84471523e025
jstorra <131458634+jstorra@users.noreply.github.com>
2023-09-24 18:31:56 -0500

    refactor: :recycle: Codigo mejorado
    Se hace cambios en el codigo para mejorar su funcionamiento, sí no hay mas pokemones para cargar el boton de la parte inferior desaparecera

--- main.js
+++ main.js
@@ -7,21 +7,17 @@ const api = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=500";
 addEventListener("DOMContentLoaded", async () => {
   let resNext = await loadCards(main, api);
   loadActions();
-  if (!resNext) {
-    btnMore.style.display = "none";
-  } else {
-    btnMore.addEventListener("click", async () => {
-      console.log(resNext);
-      const res = await (await fetch(resNext)).json();
-      const btns = await Promise.all(
-        res.results.map(async (date) => {
-          const namePokemon = date.name;
-          const res1 = await (
-            await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`)
-          ).json();
-          const img = res1.sprites.front_default;
-          const defaultImg = "assets/img/pokeBall.gif";
-          return `<div class="containerCard">
+  btnMore.addEventListener("click", async () => {
+    const res = await (await fetch(resNext)).json();
+    const btns = await Promise.all(
+      res.results.map(async (date) => {
+        const namePokemon = date.name;
+        const res1 = await (
+          await fetch(`https://pokeapi.co/api/v2/pokemon/${namePokemon}`)
+        ).json();
+        const img = res1.sprites.front_default;
+        const defaultImg = "assets/img/pokeBall.gif";
+        return `<div class="containerCard">
                       <div id="${date.name}" class="btnPokemon">
                           <div class="imgPokemon">
                               <img src="${img ? img : defaultImg}">
@@ -31,13 +27,12 @@ addEventListener("DOMContentLoaded", async () => {
                           </div>
                       </div>
                   </div>`;
-        })
-      );
-      btns.forEach((el) => {
-        main.insertAdjacentHTML("beforeend", el);
-      });
-      loadActions();
-      resNext = res.next;
+      })
+    );
+    btns.forEach((el) => {
+      main.insertAdjacentHTML("beforeend", el);
     });
-  }
+    loadActions();
+    !res.next ? (btnMore.style.display = "none") : (resNext = res.next);
+  });
 });
